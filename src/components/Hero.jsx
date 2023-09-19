fd344d49b8246cd3115e16d72617a9124273adb5
nishamvp <107843151+nishamvp@users.noreply.github.com>
2023-09-19 14:02:24 +0530

    Hero and computer component is edited..

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -22,7 +22,7 @@ const Hero = () => {
         </div>
       </div>
       <ComputersCanvas />
-      <div className='absolute   w-full flex justify-center items-start'>
+      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href="#about">
           <div className='w-[35px] h-[64px] rounded-3xl flex justify-center items-start border-4 border-secondary p-2'>
             <motion.div
@@ -32,7 +32,7 @@ const Hero = () => {
             transition={{
               duration:1.5,
               repeat:Infinity,
-              repeatType:'reverse'
+              repeatType:'loop'
             }}
             className='h-3 w-3 rounded-full bg-secondary mb-1' 
             />
